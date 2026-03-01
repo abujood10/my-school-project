8c4440dd197b32cc7e5d47cefa518730f689e383
Abbas <amjamj10@gmail.com>
2026-03-02 00:34:56 +0300

    Fix PocketBase URL

--- lib/pocketbase.ts
+++ lib/pocketbase.ts
@@ -1,8 +1,10 @@
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase("http://127.0.0.1:8090");
+const pb = new PocketBase(
+  process.env.NEXT_PUBLIC_PB_URL
+);
 
-// مهم: حفظ الجلسة
+// مهم: منع إلغاء الطلبات تلقائياً
 pb.autoCancellation(false);
 
 export default pb;
